8e1019d9950245c1a35d44287c5bc2263c32a03f
zhangliang25m <zhangliang25m@163.com>
2020-09-29 16:41:02 +0800

    基本操作

--- CsharpThrad/Chapter1/Form1.Designer.cs
+++ CsharpThrad/Chapter1/Form1.Designer.cs
@@ -28,13 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.txtRead = new System.Windows.Forms.TextBox();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btnSingleThread = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtRead
+            // 
+            this.txtRead.Location = new System.Drawing.Point(25, 370);
+            this.txtRead.Multiline = true;
+            this.txtRead.Name = "txtRead";
+            this.txtRead.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtRead.Size = new System.Drawing.Size(683, 154);
+            this.txtRead.TabIndex = 13;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(15, 124);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(107, 23);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "2、暂停线程";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // btnSingleThread
+            // 
+            this.btnSingleThread.Location = new System.Drawing.Point(15, 72);
+            this.btnSingleThread.Name = "btnSingleThread";
+            this.btnSingleThread.Size = new System.Drawing.Size(107, 23);
+            this.btnSingleThread.TabIndex = 0;
+            this.btnSingleThread.Text = "2、多线程调用";
+            this.btnSingleThread.UseVisualStyleBackColor = true;
+            this.btnSingleThread.Click += new System.EventHandler(this.btnSingleThread_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(15, 207);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(107, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "4、终止线程";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(15, 165);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(107, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "3、等待线程";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.btnNormal);
+            this.groupBox1.Controls.Add(this.button8);
+            this.groupBox1.Controls.Add(this.btnSingleThread);
+            this.groupBox1.Controls.Add(this.button6);
+            this.groupBox1.Controls.Add(this.button7);
+            this.groupBox1.Location = new System.Drawing.Point(25, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(683, 317);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "线程的基础操作：";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(143, 77);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(449, 12);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "多线程执行（子线程），在执行过程不执行其他操作，比如拖动窗体，解决程序假死";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(143, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(461, 12);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "单线程执行（主线程），在执行过程不能执行其他操作，比如拖动窗体，造成程序假死";
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Location = new System.Drawing.Point(15, 29);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(107, 23);
+            this.btnNormal.TabIndex = 8;
+            this.btnNormal.Text = "1、普通调用";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            this.btnNormal.Click += new System.EventHandler(this.btnNormal_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 344);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "响应结果：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(738, 558);
+            this.Controls.Add(this.txtRead);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "线程的基本操作";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox txtRead;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button btnSingleThread;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnNormal;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- CsharpThrad/Chapter1/Form1.cs
+++ CsharpThrad/Chapter1/Form1.cs
@@ -5,16 +5,91 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Chapter1
 {
+    /// <summary>
+    /// 官方文档：https://docs.microsoft.com/zh-cn/
+    /// 线程: https://docs.microsoft.com/zh-cn/dotnet/api/system.threading.thread?view=netcore-3.1
+    /// </summary>
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        #region 正常主线程调用:单线程访问
+        /// <summary>
+        /// 单线程：程序假死情况，在主线程执行完的情况下，你不能执行其他事情，比如拖动窗体
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnNormal_Click(object sender, EventArgs e)
+        {
+            printNumbers();
+        }
+        #endregion
+
+        #region 多线程调用
+        /// <summary>
+        /// 单线程访问：
+        /// 异常：System.InvalidOperationException:“线程间操作无效: 从不是创建控件“txtRead”的线程访问它。”
+        /// 原因：txtRead是主线程创建的，printNumbers在另一个线程t1中执行，修改主线程的控件，跨线程操作，异常
+        ///       c#竞争跨线程访问
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSingleThread_Click(object sender, EventArgs e)
+        {
+            Thread.CurrentThread.Name = "Main";
+            printNumbers();
+
+            Thread thread = new Thread(printNumbers);
+            thread.Name = "t1";
+            thread.IsBackground = true;
+            thread.Start();
+        }
+        #endregion
+
+        #region 前台、后台线程
+
+        #endregion
+
+
+
+        #region public method
+        /// <summary>
+        /// 输出数字
+        /// </summary>
+        private void printNumbers()
+        {
+            printMsg("当前运行线程名称：" + Thread.CurrentThread.Name);
+            for (int i = 1; i < 10000; i++)
+            {
+                printMsg(i.ToString());
+            }
+            txtRead.AppendText("执行完成...");
+        }
+
+        private void printMsg(string msg)
+        {
+            txtRead.AppendText(DateTime.Now.ToString("HH:mm:ss") + " " + Thread.CurrentThread.Name + "     " + msg + "\r\n");
+        }
+
+        #endregion
+
+        /// <summary>
+        /// 不让检查跨线程调用：
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
         }
     }
 }
